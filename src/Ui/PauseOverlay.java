0ab15c0ae7b6923b389fe30d160f49692d4c3ce2
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-10-26 14:05:03 +0200

    img s added for pause menu

---
+++ src/Ui/PauseOverlay.java
@@ -0,0 +1,39 @@
+package Ui;
+
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
+public class PauseOverlay {
+
+	public PauseOverlay() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void mouseDragged() {
+		
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -13,6 +13,7 @@ public class Playing extends State implements Statemethods{
 	
 	private Player player;
 	private LevelManager levelManager;
+	private boolean paused;
 	
 	public Playing(Game game) {
 		super(game);
