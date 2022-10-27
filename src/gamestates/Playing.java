fe20d0c4f24a0b503ce6224e744c3b9a28237910
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-10-27 18:13:08 +0200

    big clouds added

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import Levels.LevelManager;
 import Ui.PauseOverlay;
@@ -27,9 +28,13 @@ public class Playing extends State implements Statemethods{
 	private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;
 	private int maxLvlOffsetX = maxTilesOffset * Game.TILES_SIZE;
 	
+	private BufferedImage backgroundImg;
+	
 	public Playing(Game game) {
 		super(game);
 		initClasses();
+		
+		backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PLAYING_BG_IMG);
 	}	
 	
 	private void initClasses() {
@@ -74,6 +79,8 @@ public class Playing extends State implements Statemethods{
 
 	@Override
 	public void draw(Graphics g) {
+		
+		g.drawImage(backgroundImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
 		
 		levelManager.draw(g, xLvlOffset);
 		player.render(g, xLvlOffset);
