39aae6137cb88c2a2d7213d4f46d6848150ebef8
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-11-01 18:43:25 +0100

    game over class created

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -118,6 +118,10 @@ public class Playing extends State implements Statemethods{
 		for(int i = 0; i < smallCloudsPos.length; i++)
 		g.drawImage(smallCloud, SMALL_CLOUD_WIDTH * 4 * i - (int)(xLvlOffset * 0.7), smallCloudsPos[i], SMALL_CLOUD_WIDTH, SMALL_CLOUD_HEIGHT, null);
 	}
+	
+	public void resetAll() {
+		//TODO: reset playing, enemy etc...
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
