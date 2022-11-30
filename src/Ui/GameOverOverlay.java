f2b13a51caf6f480a3c6b534ed0ee20ae4091adc
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-11-30 20:54:37 +0100

    death screen added

--- src/Ui/GameOverOverlay.java
+++ src/Ui/GameOverOverlay.java
@@ -3,29 +3,42 @@ package Ui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import gamestates.Gamestate;
 import gamestates.Playing;
 import main.Game;
+import utilz.LoadSave;
 
 public class GameOverOverlay {
 
 	private Playing playing;
+	private BufferedImage img;
+	private int imgX, imgY, imgW, imgH;
 	
 	public GameOverOverlay(Playing playing) {
 		
 		this.playing = playing;
+		createImg();
 		
 	}
 	
+	private void createImg() {
+		
+		img = LoadSave.GetSpriteAtlas(LoadSave.DEATH_SCREEN);
+		imgW = (int) (img.getWidth() * Game.SCALE);
+		imgH = (int) (img.getHeight() * Game.SCALE);
+		imgX = Game.GAME_WIDTH / 2 - imgW / 2;
+		imgY = (int) (100 * Game.SCALE);
+		
+	}
+
 	public void draw(Graphics g) {
 		
 		g.setColor(new Color(0, 0, 0, 200));
 		g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
 		
-		g.setColor(Color.white);
-		g.drawString("GAME OVER", Game.GAME_WIDTH / 2, 150);
-		g.drawString("Press esc to enter Main Menu", Game.GAME_WIDTH / 2, 300);
+		g.drawImage(img, imgX, imgY, imgW, imgH, null);
 				
 	}
 	
